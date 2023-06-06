9d21e9835f9d31a38d07f3c92e21fe68b9ba4e58
billguo07@yahoo.ca <billguo07@yahoo.ca>
2023-06-06 16:38:46 -0600

    Initial commit

---
+++ scrappingtutorial/scrappingtutorial/spiders/quotes_spider.py
@@ -0,0 +1,19 @@
+import scrapy
+from ..items import ScrappingtutorialItem
+class QuoteSpider(scrapy.Spider):
+    name = 'quotes'
+    start_urls = ['https://quotes.toscrape.com/']
+
+    def parse(self, response, **kwargs):
+        items = ScrappingtutorialItem()
+        all_div_quotes = response.css('div.quote')
+
+        for quotes in all_div_quotes:
+            title = quotes.css('span.text::text').extract()
+            author = quotes.css('.author::text').extract()
+            tag = quotes.css('.tag::text').extract()
+
+            items['title'] = title
+            items['author'] = author
+            items['tag'] = tag
+            yield items
